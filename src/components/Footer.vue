d7b5e1ed173421daa4b0596677993e33f91b8788
Febe Calderon <samcalma1@gmail.com>
2022-04-07 16:39:18 +0200

    macroaree

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <div class="logo">
+      <img src="../img/dc-logo.png" alt="dc-logo" />
+    </div>
+    <div class="nav-bar">
+      <ul id="nav-links">
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Lorem</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello {
+  display: flex;
+  justify-content: space-around;
+}
+#nav-list li {
+  list-style: none;
+}
+</style>
